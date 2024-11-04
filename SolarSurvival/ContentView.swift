bda8fcdedf1b20f664a6ee2c75eae64a822e299e
Bryan Nguyen <bryannguyenngo@gmail.com>
2024-11-04 11:29:41 +0800

    Initial Commit

---
+++ SolarSurvival/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SolarSurvival
+//
+//  Created by Bryan Nguyen on 4/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SolarSurvival/SolarSurvivalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSurvivalApp.swift
+//  SolarSurvival
+//
+//  Created by Bryan Nguyen on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SolarSurvivalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
