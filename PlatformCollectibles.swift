ab6415ca0e798f38088165aa7e08b0722928019c
Bryan Nguyen <bryannguyenngo@gmail.com>
2024-11-09 16:01:19 +0800

    created second screen

---
+++ PlatformCollectibles.swift
@@ -0,0 +1,18 @@
+//
+//  PlatformCollectibles.swift
+//  SolarSurvival
+//
+//  Created by Bryan Nguyen on 8/11/24.
+//
+
+import SwiftUI
+
+struct PlatformCollectibles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlatformCollectibles()
+}
