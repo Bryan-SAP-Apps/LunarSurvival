f2e4bc87b066714481e50c2c6095a2c76beb0c10
28tedtan <28tedtan@gmail.com>
2024-11-11 16:51:37 +0800

    MainPage

---
+++ SolarSurvival/MainPage.swift
@@ -0,0 +1,18 @@
+//
+//  MainPage.swift
+//  SolarSurvival
+//
+//  Created by Ted Tan on 11/11/24.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPage()
+}
