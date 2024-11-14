1e37661afcaf7434612db8ec81a64945c448f14e
Ou Yanxi <yanxi.ou09@rafflesgirlssch.edu.sg>
2024-11-14 10:15:49 +0800

    added vid, restored mainpage

--- SolarSurvival/MainPage.swift
+++ SolarSurvival/MainPage.swift
@@ -7,12 +7,86 @@
 
 import SwiftUI
 
-struct MainPage: View {
+struct SwiftUIView: View {
+    @State private var isNavigated = false // State variable to control navigation
+    @State var continuee = false
+    @State var start = "Start"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Color(red: 0.3, green: 0.1, blue: 0.82)
+                .ignoresSafeArea()
+                .overlay(
+                    VStack{
+                        VStack{
+                            HStack{
+                                Text("Lunar Survival")
+                                    .font(.system(size: 50))
+                                    .bold()
+                                    .foregroundStyle(.white)
+                                
+                                Spacer()
+                            }
+                        }
+                        
+                        HStack{
+                            Spacer()
+                            VStack{
+                                
+                                // Button to navigate to the second view
+                                Button(action: {
+                                    isNavigated = true // Set the state to true to navigate
+                                    if (continuee == false){
+                                        start = "Start"
+                                    } else {
+                                        start = "Continue"
+                                    }
+                                }) {
+                                    Text(start)
+                                        .font(.title)
+                                        .padding()
+                                        .padding(.trailing, 62.5)
+                                        .padding(.leading, 62.5)
+                                        .background(Color.green)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(10)
+                                    
+                                }
+                                
+                                Button(action: {
+                                    isNavigated = true // Set the state to true to navigate
+                                    if (continuee == false){
+                                        start = "Start"
+                                    } else {
+                                        start = "Continue"
+                                    }
+                                }) {
+                                    Text("Reset")
+                                        .font(.title)
+                                        .padding()
+                                        .padding(.trailing, 55)
+                                        .padding(.leading, 60)
+                                        .background(Color.red)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(10)
+                                    
+                                        
+                                    
+                                }
+                            }
+                        }
+                        
+                    }
+                )
+            
+            // Programmatic NavigationLink
+            NavigationLink(destination: ContentView(), isActive: $isNavigated) {
+                EmptyView() // Use an empty view for the link
+            }
+        }
     }
 }
 
 #Preview {
-    MainPage()
+    SwiftUIView()
 }
