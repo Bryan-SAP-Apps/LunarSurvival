ef68da2022c95122581974f1ed186e1d0b752d7a
Ou Yanxi <yanxi.ou09@rafflesgirlssch.edu.sg>
2024-11-14 09:33:20 +0800

    Images

---
+++ SolarSurvival/Preview Content/GameHome.swift
@@ -0,0 +1,18 @@
+//
+//  GameHome.swift
+//  SolarSurvival
+//
+//  Created by Ted Tan on 14/11/24.
+//
+
+import SwiftUI
+
+struct GameHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameHome()
+}
