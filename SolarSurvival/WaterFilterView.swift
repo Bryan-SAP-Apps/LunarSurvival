6603a17da932e23df6de623fa386389fda5a0db6
Bryan Nguyen <bryannguyenngo@gmail.com>
2024-11-19 21:22:11 +0800

    changed waterfilterview

---
+++ SolarSurvival/WaterFilterView.swift
@@ -0,0 +1,18 @@
+//
+//  WaterFilterView.swift
+//  SolarSurvival
+//
+//  Created by Bryan Nguyen on 18/11/24.
+//
+
+import SwiftUI
+
+struct WaterFilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WaterFilterView()
+}
